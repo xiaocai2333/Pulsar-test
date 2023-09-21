f0a846750e699a28d8438c5e8686fe5b312acd70
cai.zhang <cai.zhang@zilliz.com>
2023-09-21 15:12:20 +0800

    Update produce
    Signed-off-by: cai.zhang <cai.zhang@zilliz.com>

--- main.go
+++ main.go
@@ -3,25 +3,60 @@ package main
 import (
 	"fmt"
 	"github.com/apache/pulsar-client-go/pulsar"
+	"os"
 	"pulsar-test/producer"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
 	//opts := pulsar.ClientOptions{URL: "pulsar://10.102.6.53:6650"}
-	opts := pulsar.ClientOptions{URL: "pulsar://172.17.0.8:6650"}
-	//pulsar://zc-pulsar-1-broker-0.zc-pulsar-1-broker.my-namespace.svc.cluster.local:6650
+	args := os.Args
+	fmt.Println("program name: ", args[0])
+	if len(args) != 6 {
+		fmt.Println("invalid args")
+		return
+	}
+	for _, arg := range args {
+		fmt.Println("arg: ", arg)
+	}
+	pulsarAddr := args[1]
+	topicPrefix := args[2]
+	goroutinesNumStr := args[3]
+	durationStr := args[4]
+	goroutinesNum, err := strconv.Atoi(goroutinesNumStr)
+	sameTopic := args[5]
+	if err != nil {
+		fmt.Println("goroutineNum invalid: ", err)
+		return
+	}
+	durationNum, err := strconv.Atoi(durationStr)
+	if err != nil {
+		fmt.Println("goroutineNum invalid: ", err)
+		return
+	}
+	//opts := pulsar.ClientOptions{URL: "pulsar://172.17.0.8:6650"}
+	opts := pulsar.ClientOptions{URL: pulsarAddr}
 	client, err := pulsar.NewClient(opts)
 	if err != nil {
 		fmt.Printf("create client err %s", err)
 		return
 	}
-	topic := "pulsar-test-1"
-	//sub1 := "sub1"
-	//sub2 := "sub2"
-	producer.Produce(client, topic)
-	//go consumer.Consume(client, topic, sub1, 1)
-	//go consumer.Consume(client, topic, sub2, 6000)
-	//msgID := producer.ProduceForSeek(client, topic)
-	//consumer.Seek(client, topic, sub1, msgID)
-	//time.Sleep(30 * time.Second)
+
+	var wg sync.WaitGroup
+	duration := time.Minute * time.Duration(durationNum)
+
+	for i := 0; i < goroutinesNum; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			topicName := topicPrefix
+			if sameTopic == "false" {
+				topicName = topicPrefix + "-" + strconv.Itoa(i)
+			}
+			producer.Produce(client, topicName, duration)
+		}()
+	}
 }
--- producer/producer.go
+++ producer/producer.go
@@ -3,104 +3,45 @@ package producer
 import (
 	"context"
 	"fmt"
-	"strconv"
-	"sync"
 	"time"
 
 	"github.com/apache/pulsar-client-go/pulsar"
 )
 
-var client pulsar.Client
-var wg sync.WaitGroup
-
-func main() {
-	opts := pulsar.ClientOptions{URL: "pulsar://localhost:6650"}
-	var err error
-	client, err = pulsar.NewClient(opts)
-	if err != nil {
-		fmt.Printf("create client err %s", err)
-	}
-	wg = sync.WaitGroup{}
-	cnt := 5
-	wg.Add(cnt)
-	for i := 0; i < cnt; i++ {
-		go Produce(client, "input-api")
-	}
-	wg.Wait()
-}
-
-func Produce(client pulsar.Client, topic string) {
+func Produce(client pulsar.Client, topic string, duration time.Duration) {
 	//defer wg.Done()
 	opts := pulsar.ProducerOptions{Topic: topic}
 	producer, err := client.CreateProducer(opts)
 	if err != nil {
 		fmt.Printf("create producer err %s", err)
 	}
-	msgSize := 3 * 1024 * 1024
+	msgSize := 1 * 1024 * 1024
 	strBytes := make([]byte, msgSize)
 	for j := 0; j < msgSize; j++ {
 		strBytes[j] = byte(j)
 	}
-
-	timer := time.NewTimer(60 * time.Minute)
-	defer timer.Stop()
-	goroutineNum := 6
+	ticker := time.NewTicker(duration)
+	defer ticker.Stop()
+	msg := &pulsar.ProducerMessage{
+		Payload:    strBytes,
+		Properties: map[string]string{},
+	}
+	messageNum := 0
+	start := time.Now().UnixMilli()
 	//var wg sync.WaitGroup
-	for i := 0; i < goroutineNum; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			msg := &pulsar.ProducerMessage{
-				Payload:    strBytes,
-				Properties: map[string]string{},
-			}
-			start := time.Now().UnixMilli()
-			for j := 0; j < 100; j++ {
-				_, err = producer.Send(context.Background(), msg)
-				if err != nil {
-					fmt.Printf("producer send error %s", err)
-				}
-			}
-			end := time.Now().UnixMilli()
-			fmt.Printf("send 100 msg cost: %d, %f\n", end-start, float64(int64(100*msgSize)/(end-start))*1000.0/1024/1024)
-		}()
-	}
-	wg.Wait()
-}
-
-func ProduceForSeek(client pulsar.Client, topic string) pulsar.MessageID {
-	//defer wg.Done()
-	opts := pulsar.ProducerOptions{Topic: topic}
-	producer, err := client.CreateProducer(opts)
-	if err != nil {
-		fmt.Printf("create producer err %s", err)
-		panic(err)
-	}
-	i := 0
-	//timer := time.NewTimer(60 * time.Minute)
-	//defer timer.Stop()
-	fmt.Println("produce data...")
-	var msgID2 pulsar.MessageID
 	for {
-		i++
-		str := "ahahahaha" + strconv.Itoa(i)
-		fmt.Printf("i := %d, insert data: %s \n", i, str)
-		msg := &pulsar.ProducerMessage{
-			Payload:    []byte(str),
-			Properties: map[string]string{},
-		}
-		msgID, err := producer.Send(context.Background(), msg)
-		if err != nil {
-			fmt.Printf("producer send error %s", err)
-			panic(err)
-		}
-		if i == 10 {
-			msgID2 = msgID
-		}
-		if i == 20 {
-			break
+		select {
+		case <-ticker.C:
+			end := time.Now().UnixMilli()
+			fmt.Printf("send 100 msg cost: %d, %f\n", end-start, float64(int64(messageNum*msgSize)/(end-start))*1000.0/1024/1024)
+			return
+		default:
+			_, err = producer.Send(context.Background(), msg)
+			if err != nil {
+				fmt.Printf("producer send error %s", err)
+				return
+			}
+			messageNum++
 		}
 	}
-	fmt.Println("msgID2", msgID2)
-	return msgID2
 }
